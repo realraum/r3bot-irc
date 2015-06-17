89628995d4c5494fe3fe9b69080b99cdd0533340
stefan <dev@2904.cc>
2015-06-17 20:28:06 +0200

    implemented quick&dirty mjam 'API' client

---
+++ mjam.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import re
+from BeautifulSoup import BeautifulSoup
+
+
+class Mjam():
+
+    def __init__(self, url, cookies=None):
+        self.session = requests.Session()
+        self.url = url
+        if cookies is not None:
+            self.session.cookies = requests.utils.cookiejar_from_dict(cookies)
+
+    def loadOrder(self):
+        self.index = self.session.get(url=self.url)
+        return self.index
+
+    def isOrderSubmitted(self):
+        m = re.search('.*deine Bestellung, (.*)!.*', self.index.text)
+        return m is not None
+
+    def isOrderGone(self):
+        return self.index.status_code == 403
+
+    def getRestaurantName(self):
+        parsed_html = BeautifulSoup(self.index.text)
+        self.restaurant_name = parsed_html.body.find(
+            'h1', attrs={'property': 'v:name'}).text
+        return self.restaurant_name
+
+    def getOrderNumer(self):
+        parsed_html = BeautifulSoup(self.index.text)
+        self.order_number = parsed_html.body.find(
+            'input', attrs={'name': 'order_number'}).get('value')
+        return self.order_number
+
+    def getOrderer(self):
+        m = re.search('.*deine Bestellung, (.*)!.*', self.index.text)
+        return m.group(1)
+
+    def loadOrderETA(self):
+        orderstatus_url = "https://www.mjam.net/ajax/customer/order/info/" + \
+            self.order_number
+        orderstatus_resp = self.session.post(url=orderstatus_url)
+        orderstatus = json.loads(orderstatus_resp.text)
+        m = re.search(
+            '.*sollte um (.*) ankommen.$', orderstatus['short_description'])
+        self.order_eta = m.group(1)
+        return self.order_eta
